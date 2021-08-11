d1ea64b86f9f100649add402b2b2332d8d9a3880
BhargaviNadimpalli <bhargavinadimpalli423@gmail.com>
2021-08-11 13:58:31 -0600

    Nlog program

---
+++ NLogDemo/NLog.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace NlogSession
+{
+    class Nlog
+    {
+        private static readonly Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        public void LogDebug(string message)
+        {
+            logger.Debug(message);
+        }
+        public void LogError(string message)
+        {
+            logger.Error(message);
+        }
+        public void LogInfo(String message)
+        {
+            logger.Info(message);
+        }
+        public void LogWarn(String message)
+        {
+            logger.Warn(message);
+        }
+    }
+}
---
+++ NLogDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NlogSession
+{
+    class Program
+    {
+        private static string result;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Addition on numbers \n Enter values : " + result);
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            var add = new AddNumbers();
+            Console.WriteLine("Result is " + " " + add.Sum(firstNumber, secondNumber));
+
+        }
+    }
+}
